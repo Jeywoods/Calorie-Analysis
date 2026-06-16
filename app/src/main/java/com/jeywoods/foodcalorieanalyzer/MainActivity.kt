6a9efc03a1261b59973c69a76cedf216e065cd3c
Jeywoods <VladSagaydak1@mail.ru>
2026-06-17 02:31:32 +0300

    added onboarding

--- app/src/main/java/com/jeywoods/foodcalorieanalyzer/MainActivity.kt
+++ app/src/main/java/com/jeywoods/foodcalorieanalyzer/MainActivity.kt
@@ -6,9 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.compose.rememberNavController
 import com.jeywoods.foodcalorieanalyzer.presentation.navigation.NavGraph
+import com.jeywoods.foodcalorieanalyzer.presentation.onboarding.OnboardingManager
+import com.jeywoods.foodcalorieanalyzer.presentation.onboarding.OnboardingScreen
 import com.jeywoods.foodcalorieanalyzer.ui.theme.FoodAnalyzerTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,8 +25,22 @@ class MainActivity : ComponentActivity() {
         setContent {
             FoodAnalyzerTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
-                    val navController = rememberNavController()
-                    NavGraph(navController = navController)
+                    val context = LocalContext.current
+                    var showOnboarding by remember {
+                        mutableStateOf(!OnboardingManager.isOnboardingDone(context))
+                    }
+
+                    if (showOnboarding) {
+                        OnboardingScreen(
+                            onFinish = {
+                                OnboardingManager.setOnboardingDone(context)
+                                showOnboarding = false
+                            }
+                        )
+                    } else {
+                        val navController = rememberNavController()
+                        NavGraph(navController = navController)
+                    }
                 }
             }
         }
